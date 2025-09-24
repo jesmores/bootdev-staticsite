b6357806c5ad3c75c7ad86dc1b91882d0fa2ce77
jesmores <125214+jesmores@users.noreply.github.com>
2025-09-24 11:21:57 -0700

    initial impl of publish routine in main

--- src/main.py
+++ src/main.py
@@ -1,23 +1,81 @@
-from textnode import *
-from htmlnode import *
+import os
+import shutil
+import sys
 
 
 def scratchpad():
-    """Fill me in to try scenarios manually. Print or return something to see output when run from CLI."""
-    node = ParentNode(
-        "p",
-        [
-            LeafNode("b", "Bold Text"),
-            LeafNode(None, "Normal Text"),
-            LeafNode("i", "Italic Text", props={"class": "italic"}),
-            LeafNode(None, " More Normal Text")
-        ]
-    )
-    return node.to_html()
+    pass
+
+
+def clear_directory(path:str) -> bool:
+    """Given a directory path, clear its contents"""
+    # print(f"Clearing directory: {path}")
+    if not os.path.exists(path):
+        return False
+
+    for path_item in os.listdir(path):
+        item_path = os.path.join(path, path_item)
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+        elif os.path.isdir(item_path):
+            if not clear_directory(item_path):
+                return False
+            os.rmdir(item_path)
+
+    return True
+
+
+def copy_directory(src:str, dst:str) -> bool:
+    """Copy the contents of one directory to another"""
+    # print(f"Copying from {src} to {dst}")
+    if not os.path.exists(src):
+        return False
+
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
+    for item in os.listdir(src):
+        src_item = os.path.join(src, item)
+        dst_item = os.path.join(dst, item)
+
+        if os.path.isdir(src_item):
+            if not copy_directory(src_item, dst_item):
+                return False
+        else:
+            shutil.copy2(src_item, dst_item)
+
+    return True
+
+def prepare_directory(src:str, dst:str) -> bool:
+    """Prepare a directory by clearing it and copying new contents"""
+    # first make sure src and dst are absolute paths
+    if not os.path.isabs(src) or not os.path.isabs(dst):
+        raise ValueError("Both src and dst must be absolute paths")
+
+    # next clear and then copy src to dst
+    if not clear_directory(dst):
+        return False
+    if not copy_directory(src, dst):
+        return False
+
+    return True
 
 
 def main():
-    pass
+    # first determine what our script's current directory is
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    base_dir = os.path.dirname(script_dir)
+    # next build the paths we need
+    public_dir = os.path.join(base_dir, "public")
+    static_dir = os.path.join(base_dir, "static")
+
+    try:
+        if not prepare_directory(static_dir, public_dir):
+            print("Failed to prepare the public directory")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error preparing directories: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
